3f7e360301dcf7b2345600d1c33559a57436c143
Diego Tenjo <tenjodiego961@gmail.com>
2025-03-27 04:23:27 -0500

    Add images endpoints

--- back/src/routes/storageRoutes.js
+++ back/src/routes/storageRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { upload as uploadController  } from '../controllers/storageController.js';
+import { upload as uploadController, images, getImagesById } from '../controllers/storageController.js';
 
 const router = express.Router();
 
 router.post('/upload', uploadController);
+router.get("/images", images);
+router.get("/get-image", getImagesById);
 
 router.get('/', (req, res) => {
     res.status(200).json({ message: 'Holaaa' });
